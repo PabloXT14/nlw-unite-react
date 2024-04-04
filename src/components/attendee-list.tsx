ed2c0f5c11d9f95e698d37b90dcdbaf5c00ba557
Pablo Alan <pabloxt14@gmail.com>
2024-04-04 03:27:48 -0300

    feat: fetch datas from api

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -11,35 +11,100 @@ import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
-import { ChangeEvent, useState } from 'react'
-import { attendees } from '../data/attendees'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { formatDistance } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
-export function AttendeeList() {
-  const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+type Attendee = {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+  checkedInAt: string | null
+}
 
-  const totalPages = Math.ceil(attendees.length / 10)
+export function AttendeeList() {
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+  const [total, setTotal] = useState(0)
+  const [attendees, setAttendees] = useState<Attendee[]>([])
+
+  const totalPages = Math.ceil(total / 10)
+
+  useEffect(() => {
+    const url = new URL(
+      'http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees',
+    )
+
+    url.searchParams.set('pageIndex', String(page - 1))
+
+    if (search.length > 0) {
+      url.searchParams.set('query', search)
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setAttendees(data.attendees)
+        setTotal(data.total)
+      })
+  }, [page, search])
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('search', search)
+
+    window.history.pushState({}, '', url)
+
+    setSearch(search)
+  }
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('page', String(page))
+
+    window.history.pushState({}, '', url)
+
+    setPage(page)
+  }
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   return (
@@ -51,6 +116,7 @@ export function AttendeeList() {
           <input
             type="text"
             placeholder="Buscar participante..."
+            value={search}
             onChange={onSearchInputChanged}
             className="flex-1 border-0 bg-transparent p-0 text-sm focus:ring-0"
           />
@@ -78,9 +144,9 @@ export function AttendeeList() {
           </TableRow>
         </thead>
         <tbody>
-          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => (
+          {attendees.map((attendee) => (
             <TableRow
-              key={`${attendee.name}-${attendee.id}`}
+              key={attendee.id}
               className="transition-all hover:bg-white/5"
             >
               <TableCell>
@@ -105,10 +171,14 @@ export function AttendeeList() {
                 })}
               </TableCell>
               <TableCell>
-                {formatDistance(attendee.checkedInAt, new Date(), {
-                  addSuffix: true,
-                  locale: ptBR,
-                })}
+                {attendee.checkedInAt === null ? (
+                  <span className="text-zinc-400">Não fez check-in</span>
+                ) : (
+                  formatDistance(attendee.checkedInAt, new Date(), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })
+                )}
               </TableCell>
               <TableCell>
                 <IconButton transparent>
@@ -121,7 +191,7 @@ export function AttendeeList() {
         <tfoot>
           <TableRow>
             <TableCell colSpan={3}>
-              Mostrando 10 de {attendees.length} itens
+              Mostrando {attendees.length} de {total} itens
             </TableCell>
             <TableCell colSpan={3} className="text-right">
               <div className="inline-flex items-center gap-8">
